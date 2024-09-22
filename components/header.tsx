e992085b432f724afcc3a278b57673762b53e447
Derrick Ankrah <darhin97@gmail.com>
2024-09-22 14:32:50 +0000

    fix mobile nav

--- components/header.tsx
+++ components/header.tsx
@@ -4,9 +4,7 @@ import Navigation from "@/components/navigation";
 
 const Header = () => {
   return (
-    <header
-      className={"bg-white px-4 w-full py-3 lg:px-14 sticky  top-0 z-[9999]"}
-    >
+    <header className={"bg-white px-4 w-full py-3 lg:px-14 sticky  top-0 z-30"}>
       <div className={"max-w-screen-2xl mx-auto text-black"}>
         <div className={"flex items-center justify-between w-full"}>
           <HeaderLogo />
